f54999b7dbd7b721851dbe72a8c3b0a9db77f2c3
Yuri <yuriyroudenko@gmail.com>
2024-03-15 12:35:16 +0300

    first three steps from the tutorial

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,45 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,'0')[0..4]
+
+end
+
+def legilators_by_zip_code(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: "country",
+      roles: ["legislatorUpperBody", "legislatorLowerBody"]
+      )
+
+      legislators = legislators.officials
+      legislator_names = legislators.map(&:name)
+      legislators_string = legislator_names.join(", ")
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+
+    end
+end
+
 puts 'Event Manager Initialized!'
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  names = columns[2]
-  puts names
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+  )
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legilators_by_zip_code(zipcode)
+
+  puts "#{name} - #{zipcode} - #{legislators}"
 
 end
