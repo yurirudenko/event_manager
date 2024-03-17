2b098403b2ef2cdc518ca5f83fa439b86662839d
Yuri <yuriyroudenko@gmail.com>
2024-03-17 20:26:27 +0300

    4th step of the tutorial

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
+
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,'0')[0..4]
@@ -15,15 +17,22 @@ def legilators_by_zip_code(zip)
       address: zip,
       levels: "country",
       roles: ["legislatorUpperBody", "legislatorLowerBody"]
-      )
+      ).officials
 
-      legislators = legislators.officials
-      legislator_names = legislators.map(&:name)
-      legislators_string = legislator_names.join(", ")
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
 
     end
+end
+
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
 end
 
 puts 'Event Manager Initialized!'
@@ -33,13 +42,28 @@ contents = CSV.open(
   headers: true,
   header_converters: :symbol
   )
+  template_letter = File.read('form_letter.erb')
+  erb_template = ERB.new template_letter
+
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
-  legislators = legilators_by_zip_code(zipcode)
+  phone = row[:homephone].gsub(/[- .()]/, '')
+  p phone
 
-  puts "#{name} - #{zipcode} - #{legislators}"
+
+
+
+
+  legislators = legilators_by_zip_code(zipcode)
+  # form_letter = erb_template.result(binding)
+  # save_thank_you_letter(id, form_letter)
+
+
+
+  # puts "#{name} - #{zipcode} - #{legislators}"
 
 end
